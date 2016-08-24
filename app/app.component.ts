a547a1dbe183f343007bb0883ff8af6370fdfe00
Reyes <rreyes@seic.com>
2016-08-24 12:15:11 -0400

    broke out customer info into separate component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CustomerComponent } from './customer/customer.component';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html' 
+    templateUrl: 'app/app.component.html',
+    directives: [CustomerComponent]
 })
 export class AppComponent {
     //[ ] means property binding - one way (component to DOM)
@@ -11,6 +13,13 @@ export class AppComponent {
     title = 'Customer App';
     name = 'Ramon';
     ramonsColor = 'red';
+    customers = [
+        {id: 1, name: 'Ramon'},
+        {id: 2, name: 'Kevin'},
+        {id: 3, name: 'Sanjay'},
+        {id: 4, name: 'Paul'},
+        {id: 5, name: 'Esmeralda'}
+    ];
 
     changeButton() {
         this.ramonsColor = this.ramonsColor === 'green' ? 'red' : 'green';
