933442bc2b9445599f9178639350131d964e594b
Reyes <rreyes@seic.com>
2016-08-25 12:32:52 -0400

    wrapped up, created barrel file for a few imports

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { CustomersComponent } from './customer/customers.component';
+//import { CustomersComponent } from './customer/customers.component';
+//import { CustomerService } from './customer/customer.service';
+
+//Here is my index barrel
+import { CustomersComponent, CustomerService } from './customer/index';
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
     directives: [CustomersComponent],
-    providers: [HTTP_PROVIDERS]
+    providers: [HTTP_PROVIDERS, CustomerService]
 })
 export class AppComponent {
     //[ ] means property binding - one way (component to DOM)
---
+++ app/customer/index.ts
@@ -0,0 +1,4 @@
+//A barrel of customer-related "things"
+export * from './customer.service';
+export * from './customer.component';
+export * from './customers.component';
